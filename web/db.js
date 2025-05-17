2a654868eaca45e95206e06593abb10423d587ac
IgorThiagoLara <igorthiago.cabeca@gmail.com>
2025-05-17 16:52:09 -0300

    implementacao inicial web usando servidor

---
+++ web/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',     // Remova a porta daqui
+    port: 3307,            // Adicione a porta como propriedade separada
+    user: 'root',
+    password: 'root',
+    database: 'esp_biometria',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+module.exports = pool.promise();
---
+++ web/script.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS esp_biometria;
+USE esp_biometria;
+
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        matricula VARCHAR(20) NOT NULL UNIQUE,
+        telefone VARCHAR(20) NOT NULL,
+        data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );  
+
+    CREATE TABLE IF NOT EXISTS logs_biometria (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        matricula VARCHAR(20) NOT NULL,
+        status VARCHAR(50) NOT NULL,
+        origem VARCHAR(50) NOT NULL,
+        data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (matricula) REFERENCES usuarios(matricula)
+    );
